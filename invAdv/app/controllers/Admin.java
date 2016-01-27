66d5dc561d3ef7a0855d8abfc32b3d1c8e5815d2
Nermine Moussi <moussin@ensipc66.imag.fr>
2016-01-27 11:12:53 +0100

    update

--- invAdv/app/controllers/Admin.java
+++ invAdv/app/controllers/Admin.java
@@ -15,7 +15,61 @@ public class Admin extends Controller{
         }
     }
  
+    
     public static void index() {
+        String user = Security.connected();
+        List<InvestementAdvice> posts = InvestementAdvice.find("author.email", user).fetch();
+        render(posts);
+    }
+    
+    public static void form() {
         render();
     }
+     
+    public static void save() {
+        // Not implemented yet
+    }
+    
+  
+    
+    public static void save(Long id, String title, String content, String tags) {
+    	InvestementAdvice post;
+        if(id == null) {
+            // Create post
+            User author = User.find("byEmail", Security.connected()).first();
+            post = new InvestementAdvice(new Date(), title, content, author, null, 0, 0, null);
+
+        } else {
+            // Retrieve post
+        	post = InvestementAdvice.findById(id);
+            // Edit
+            post.title = title;
+            post.content = content;
+        }
+        // Set tags list
+        /*for(String tag : tags.split("\\s+")) {
+            if(tag.trim().length() > 0) {
+                post.tags.add(Tag.findOrCreateByName(tag));
+            }
+        }*/
+        // Validate
+        validation.valid(post);
+        if(validation.hasErrors()) {
+            render("@form", post);
+        }
+        // Save
+        post.save();
+        index();
+    }
+
+    
+    public static void form(Long id) {
+        if(id != null) {
+            InvestementAdvice post = InvestementAdvice.findById(id);
+            render(post);
+        }
+        render();
+    }
+
+    
 }
